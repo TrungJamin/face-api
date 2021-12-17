ff2ca887d6b3f339e2a11070ebc8c14ccdfb4cec
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2021-12-17 21:03:21 +0700

    update image

--- script.js
+++ script.js
@@ -44,21 +44,13 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = [
-    "Black Widow",
-    "Captain America",
-    "Captain Marvel",
-    "Hawkeye",
-    "Jim Rhodes",
-    "Thor",
-    "Tony Stark",
-  ];
+  const labels = ["Tony Stark"];
   return Promise.all(
     labels.map(async (label) => {
       const descriptions = [];
       for (let i = 1; i <= 3; i++) {
         const img = await faceapi.fetchImage(
-          `https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`
+          `https://raw.githubusercontent.com/TrungJamin/face-api/master/labeled_images/${label}/${i}.jpg`
         );
         const detections = await faceapi
           .detectSingleFace(img)
